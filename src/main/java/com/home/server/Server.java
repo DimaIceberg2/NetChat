c0abcafe91d294805edf132246e5f64f0a797a9f
DimaPC <email>
2022-06-06 09:37:30 +0300

    Revert to hibernate and postgres sql database

--- src/main/java/com/home/server/Server.java
+++ src/main/java/com/home/server/Server.java
@@ -7,7 +7,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 public class Server {
     private static ServerSocket serverSocket;
